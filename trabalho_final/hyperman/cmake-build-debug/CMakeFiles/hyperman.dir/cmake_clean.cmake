dfddb261310a153c23c2a749b8bc920af587a3d9
Pedro Lucas <74577150+Pedro-Lucaas@users.noreply.github.com>
2021-08-16 20:06:56 -0300

    Add files via upload

---
+++ trabalho_final/hyperman/cmake-build-debug/CMakeFiles/hyperman.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hyperman.dir/A.cpp.o"
+  "hyperman"
+  "hyperman.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hyperman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
