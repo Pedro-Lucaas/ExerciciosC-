7c5e5c15f5a951967596af7c88b1583f99c8f427
Pedro-Lucaas <pedro.plso1234@gmail.com>
2021-08-18 17:01:52 -0300

    mandrake

--- mandrake/main.cpp
+++ mandrake/main.cpp
@@ -9,22 +9,16 @@
 #include <chrono>
 #include <algorithm>
 #include <unordered_map>
+#include <unordered_set>
 #include <memory>
 //32168
 
-//#define LOCAL
+#define LOCAL
 
 using namespace std;
-class ponto{
-public:
-    uint_fast32_t x;
-    uint_fast32_t y;
-};
-class ptrString{
-    string str;
-};
-typedef unordered_map<string, shared_ptr<ponto>> table;
-typedef unordered_map<int, int> prime;
+
+typedef unordered_map<string , vector<string>> primeMap;
+typedef unordered_set<string> primeSet;
 
 int main(){
     uint_fast32_t i = 0;
@@ -33,16 +27,14 @@ int main(){
     uint_fast32_t numA;
     uint_fast32_t numB;
     string palavra;
-    string line2;
     string line1;
+    string line2;
     string line3;
     size_t posfA = 0;
     size_t posfB= 0;
     size_t posiA = 0;
     size_t posiB = 0;
-
-    table tableH;
-    table::hasher func = tableH.hash_function();
+    size_t tmp;
 
 #ifdef LOCAL
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
@@ -60,51 +52,65 @@ int main(){
     posiA = 0;
     posfB = 0;
     posiB = 0;
-    prime primeA;
-    prime primeB;
-
-    line2 += " ";
-    line3 += " ";
-//    for (int i = 0; i < numA; ++i) {
-//        posfA = line2.find(' ', posiA);
-//        palavra = line2.substr(posiA, posfA - posiA);
-//        shared_ptr<ponto> my (new ponto);
-//        my->x = i;
-//        my->y = j;
-//        tableH.insert(pair<string, shared_ptr<ponto>>(palavra, my));
-//        posiA = posfA+1;
-//    }
+    primeSet speciesA;
+    primeSet speciesB;
+    primeSet primesA;
+    primeSet primesB;
+    primeMap part;
+    line2 += ' ';
+    line3 += ' ';
     for (i=0;i < numA; i++) {
         posfA = line2.find(' ', posiA);
-        for (j = 0; j < numB; j++) {
-            posfB = line3.find(' ', posiB);
-            palavra = line2.substr(posiA, posfA - posiA) + line3.substr(posiB, posfB - posiB);
-            shared_ptr<string> ptrPal (new string);
-            auto it = tableH.find(palavra);
-            if (it == tableH.end()) {
-                shared_ptr<ponto> my (new ponto);
-                my->x = i;
-                my->y = j;
-                tableH.insert(pair<string, shared_ptr<ponto>>(palavra, my));
-            } else {
-                primeA.insert(pair<int, int>(it->second->x, 1));
-                primeA.insert(pair<int, int>(i, 1));
-                primeB.insert(pair<int, int>(it->second->y, 1));
-                primeB.insert(pair<int, int>(j, 1));
-            }
-            posiB = posfB + 1;
-        }
-        posfB = 0;
-        posiB = 0;
-        index += j;
+        speciesA.insert(line2.substr(posiA, posfA - posiA));
         posiA = posfA + 1;
     }
+    for (j = 0; j < numB; j++) {
+        posfB = line3.find(' ', posiB);
+        speciesB.insert(line3.substr(posiB, posfB - posiB));
+        posiB = posfB + 1;
+    }
 
+    for (auto &item : speciesA){
+        for (tmp = item.size()-1; tmp > 0; tmp--) {
+            palavra = item.substr(0, tmp);
+            auto it = speciesA.find(palavra);
+            if (it!= speciesA.end()){
+                string partKey = item.substr(tmp,item.size());
+                auto it2 = part.find(partKey);
+                if (it2 != part.end()){
+                    it2->second.push_back(item);
+                    it2->second.push_back(palavra);
+                }else{
+                    vector<string> primes;
+                    primes.push_back(item);
+                    primes.push_back(palavra);
+                    part.insert(pair<string, vector<string>>(partKey, primes));
+                }
+            }
+        }
+    }
+    for (auto &item : speciesB){
+        for (tmp = 1; tmp < item.size() ; ++tmp) {
+            palavra = item.substr(tmp, item.size());
+            auto it = speciesB.find(palavra);
+            if ( it!= speciesB.end() ){
+                string partkey = item.substr(0,tmp);
+                auto it2 = part.find(partkey);
+                if (it2 != part.end()){
+                    primesB.insert(item);
+                    primesB.insert(palavra);
+                    for (auto &item2 : it2->second){
+                        primesA.insert(item2);
+                    }
+                }
+            }
+        }
+    }
 
 #ifdef LOCAL
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     std::cout << "Time = " << std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count() << endl;
 #endif
-    cout << numA-primeA.size() << " " << numB-primeB.size()<< endl;
+    cout << numA-primesA.size() << " " << numB-primesB.size()<< endl;
     return 0;
 }
