6278cabf7839e46cae49b7a47fae42c74e6e8591
Pedro-Lucaas <pedro.plso1234@gmail.com>
2021-08-17 22:51:51 -0300

    inijn

--- hyperman/A.cpp
+++ hyperman/A.cpp
@@ -11,16 +11,16 @@
 #include <unordered_map>
 #include <memory>
 
-//#define LOCAL
+#define LOCAL
 
 using namespace std;
 
 class nodeG{
 public:
-    int num;
-    int height;
-    size_t size;
-    vector<shared_ptr<nodeG>> childPtr;
+    //int num = 0;
+    uint_fast32_t height = 1;
+    size_t size = 0;
+    //vector<shared_ptr<nodeG>> childPtr;
     shared_ptr<nodeG> dad;
 };
 
@@ -29,35 +29,32 @@ public:
 int recursiveF(shared_ptr<nodeG> item){
     int kill = 0;
     int tmpH = 0;
-    if (item->dad == nullptr){
-        return kill;
-    }
 
     if(item->height!=0){
-        tmpH = item->height+1;
+        tmpH = item->height;
         item->height = 0;
         kill++;
     }
-    if (item->dad->height > tmpH){
+    if (item->dad == nullptr)
         return kill;
-    }
-
+    if (item->dad->height != tmpH+1)
+        return kill;
     kill = kill + recursiveF(item->dad);
-
     return kill;
 }
 
 int main(){
     uint_fast32_t i = 0;
     uint_fast32_t j = 0;
-    int chef;
-    int power;
+    uint_fast32_t chef;
+    uint_fast32_t power;
     string line2;
     string line1;
     size_t posf = 0;
     size_t posi = 0;
-    int dad;
+    uint_fast32_t dad;
     line2 += " ";
+    uint_fast32_t kills = 0;
 
 #ifdef LOCAL
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
@@ -70,51 +67,35 @@ int main(){
     line1.erase(0,posf+1);
     power = stoi(line1);
 
-    vector<shared_ptr<nodeG>> vecLeaf;
-
     vector<shared_ptr<nodeG>> vecNode(chef+1);
     for (int k=0;k<=chef;k++){
     shared_ptr<nodeG> in (new nodeG);
     vecNode[k] = in;
-    vecNode[k]->num = k;
     }
 
     i = 2;
     posf = 0;
     posi = 0;
-    int hgt;
     shared_ptr<nodeG> atual;
     while (i <= chef) {
+        atual = vecNode[i];
         posf = line2.find(' ', posi);
         dad = stoi(line2.substr(posi, posf - posi));
-        atual = vecNode[i];
-        vecNode[dad]->childPtr.push_back(atual);
         atual->dad = vecNode[dad];
-        vecNode[dad]->size = vecNode[dad]->childPtr.size();
-        if (atual->size==0){
-            atual->height = 1;
-        }
-        while (atual->dad!= nullptr){
-            if(atual->dad->height < atual->height+1 ){
-                atual->dad->height = atual->height+1;
-            }
+        vecNode[dad]->size++;
+        while (atual->dad!= nullptr && atual->dad->height < atual->height+1){
+            atual->dad->height = atual->height+1;
             atual = atual->dad;
         }
         posi = posf + 1;
         i++;
     }
 
-    int kills = 0;
+    vector<int> vecKills ;
     for (i=1; i<=chef;i++){
         if(vecNode[i]->size == 0){
-            vecLeaf.push_back(vecNode[i]);
+            vecKills.push_back(recursiveF(vecNode[i]));
         }
-    }
-    vector<int> vecKills (vecLeaf.size());
-    i=0;
-    for (auto &item : vecLeaf){
-        vecKills[i] = recursiveF(item);
-            i++;
     }
     stable_sort(vecKills.begin(), vecKills.end());
     i=vecKills.size()-1;
@@ -122,10 +103,11 @@ int main(){
         kills +=  vecKills[i];
         i--;
     }
-    cout << kills+1 << endl;
+
 #ifdef LOCAL
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     std::cout << "Time = " << std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count() << endl;
 #endif
+    cout << kills << endl;
    return 0;
 }
